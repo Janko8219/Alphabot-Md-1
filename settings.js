3b5e40ab16e7e91074034e6417c4d4666eb590a8
DOMINIC <98521160+Janko8219@users.noreply.github.com>
2023-04-19 00:47:09 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -68,7 +68,7 @@ global.author = 'Created By Jo-jo ' + ownername //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.push_kontak_delay = 5000 //5 detik, 1000 = 1 detik
 //type menu for menu
-global.typemenu = 'templateGif' //
+global.typemenu = 'templateDocument' //
 //type menu for downloadmenu, animemenu, etc
 global.typemenu2 = 'image' //
 global.multiplier = 69
